9980fb1e079977d2a076fbdfc1a385cb0855b922
Tiến LD <ledientien2005@gmail.com>
2025-08-12 15:21:57 +0700

    add

--- src/main/java/com/ra/base_spring_boot/BaseSpringBootApplication.java
+++ src/main/java/com/ra/base_spring_boot/BaseSpringBootApplication.java
@@ -3,13 +3,11 @@ package com.ra.base_spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BaseSpringBootApplication
-{
 
-    public static void main(String[] args)
-    {
+@SpringBootApplication
+
+public class BaseSpringBootApplication {
+    public static void main(String[] args) {
         SpringApplication.run(BaseSpringBootApplication.class, args);
     }
-
 }
