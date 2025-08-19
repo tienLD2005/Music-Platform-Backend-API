e9a35a5a326df2e022f698a5c49a1d885497b307
Tiến LD <ledientien2005@gmail.com>
2025-08-19 20:04:26 +0700

    feat: update ten controller + config

--- src/main/java/com/ra/base_spring_boot/controller/ArtistLyricsController.java
+++ src/main/java/com/ra/base_spring_boot/controller/ArtistLyricsController.java
@@ -1,6 +1,5 @@
 package com.ra.base_spring_boot.controller;
 
-import com.ra.base_spring_boot.dto.req.LyricsRequest;
 import com.ra.base_spring_boot.dto.resp.LyricsResponseDTO;
 import com.ra.base_spring_boot.services.ILyricsService;
 import lombok.RequiredArgsConstructor;
@@ -10,19 +9,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/lyrics")
+@RequestMapping("/api/v1/artist/lyrics")
 @RequiredArgsConstructor
-public class LyricsController {
+public class ArtistLyricsController {
 
     private final ILyricsService lyricsService;
 
-    @PreAuthorize("hasAuthority('ROLE_ARTIST')")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LyricsResponseDTO> createLyrics(@RequestParam Long songId) {
         return ResponseEntity.ok(lyricsService.createLyrics(songId));
     }
-
-    @PreAuthorize("hasAuthority('ROLE_ARTIST')")
     @GetMapping(value = "/song/{songId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LyricsResponseDTO> getLyricsBySong(@PathVariable Long songId) {
         return ResponseEntity.ok(lyricsService.getLyricsBySong(songId));
