42b274f8c495cf509a8d3ace5f0dd830f68c99f2
Ha Duc Luong <haducluongdz@gmail.com>
2025-08-20 06:14:51 +0000

    Edit Comment.java

--- src/main/java/com/ra/base_spring_boot/model/Comment.java
+++ src/main/java/com/ra/base_spring_boot/model/Comment.java
@@ -46,10 +46,4 @@ public class Comment extends BaseObject
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @Column(nullable = false)
-    private int likes = 0;
-
-    @Column(nullable = false)
-    private boolean reported = false;
-
 }
