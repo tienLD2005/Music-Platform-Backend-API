b9b912e171cab5ddce53935fc652f16b92efa50f
CanhVeTaiGia <lvy16012005@gmail.com>
2025-08-21 13:03:53 +0700

    feat: save before update

--- src/main/java/com/ra/base_spring_boot/dto/req/SubscriptionRequestDTO.java
+++ src/main/java/com/ra/base_spring_boot/dto/req/SubscriptionRequestDTO.java
@@ -1,17 +1,24 @@
 package com.ra.base_spring_boot.dto.req;
 
-import com.ra.base_spring_boot.model.constants.PaymentMethod;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class SubscriptionRequestDTO {
-    @NotNull(message = "Plan id can't be null")
+    @NotNull(message = "Plan id can't null")
     private Long planId;
 
-    @NotNull(message = "Please select payment method")
-    private PaymentMethod paymentMethod;
+    @Pattern(
+            regexp = "^(PAYPAL|CREDIT_CARD|MOMO|ZALO_PAY)$",
+            message = "Invalid payment method"
+    )
+    private String paymentMethod;
 }
