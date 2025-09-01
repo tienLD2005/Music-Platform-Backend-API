a128ecdd681beaa3306d025ce0a9d0d6e58b7706
haducluonghnk2023 <haducluongdz@gmail.com>
2025-09-01 21:26:12 +0700

    fix: delete songreacttion manytomany

--- src/main/java/com/ra/base_spring_boot/model/User.java
+++ src/main/java/com/ra/base_spring_boot/model/User.java
@@ -124,7 +124,4 @@ public class User extends BaseObject {
     @OneToMany(mappedBy = "artist")
     private List<Song> songs;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<SongReaction> songReactions;
-
 }
